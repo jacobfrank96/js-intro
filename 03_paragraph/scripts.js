27e4847c5e84e7667c258eeedd53bc7b1769c5bb
jacobfrank96 <jacobfrank@jacobs-air.dhcp.wustl.edu>
2016-04-11 18:41:30 -0500

    commit

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,4 @@
+document.write("<h1>My third JavaScript program</h2>");
+document.write("<p> lajsd;fjaskjdf;aksjdfljaldsf asdkjfkasdjf;alksdjf a;lsdkjf;alskdjf;al aaskfjhklahsflkjhalksdjhflak aksdflkajsdflka asdkljfh aslkjdfhklajsdf sdlfkas</p>");
+document.write("<p> lajsd;fjaskjdf;aksjdfljaldsf asdkjfkasdjf;alksdjf a;lsdkjf;alskdjf;al aalksjdfla klajhsdflk akjlsdhfkla alskdhfak lkjhasdflkajh alskdjhfakl asdkjfhal sdlfkas</p>");
+document.write("<p> lajsd;fjaskjdf;aksjdfljaldsf asdkjfkasdjf;alksdjf a;lsdkjf;alskdjfalksjfak alkjhsdf  laksjdhf hfklas kasdlfkasdkjf lkashdflka ;al asdlfkas</p>");
