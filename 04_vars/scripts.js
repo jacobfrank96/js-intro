25ce5b48b0f2c733f5e435f07d7eddb864a169ee
jacobfrank96 <jacobfrank@jacobs-air.dhcp.wustl.edu>
2016-04-11 18:48:27 -0500

    commit

---
+++ 04_vars/scripts.js
@@ -0,0 +1,9 @@
+var cat= "There is a gunman on campus. Head to a safe location";
+alert(cat);
+
+var content="";
+
+content+="<h1>this is a heading </h1>";
+content+="<p>the first text area</p>";
+
+document.write(content);
